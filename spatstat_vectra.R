724fb9a991e4bd35a240876260b7b67ea7503048
Erik <erikbosch94@gmail.com>
2019-11-24 16:51:58 +0100

    implemented border corrections that now return correct values.

--- spatstat_vectra.R
+++ spatstat_vectra.R
@@ -99,7 +99,7 @@ do_analyse <- function(Intable, PhenoOrder = NULL, Cols = NULL, phenotype = NULL
 
   # define all inbuild statistics
   options = c("G","F", "J","Gdot", "Jdot", "K", "L", "pcf", "Kdot", "Ldot")
-
+  # G and F are alreay in J.
   
   values_options = list()
   
@@ -108,20 +108,20 @@ do_analyse <- function(Intable, PhenoOrder = NULL, Cols = NULL, phenotype = NULL
   for (option in options){
     # print(option)
     all_types = alltypes(csd_ppp,fun = paste(option), dataname = sample_name, envelope = FALSE)
-    # print(all_types$fns)    
+    # print(all_types$fns)
     all_types_options_sample_name[[option]] = all_types
     if (plotter[2] == TRUE){
       plot(all_types)
     }
-    print(option)
+    
     output = interpolate_r(all_types, r_vec, option)
     
-    # r_close_list = output[1]
-    # statistic_close_list = output[2]
-    stop("round 1")
+    r_close_list = output[1]
+    statistic_close_list = output[2]
+    
   }
   
-  return(c(distances, output))
+  return(c(pairwise_distances, counts_sample, density_sample, MED_min, MED, MAD_min, MAD, r_close_list, statistic_close_list))
 }
 
 
@@ -181,77 +181,80 @@ interpolate_r <- function(all_types, r_vec, option){
   statistic_close_list = list()
 
   for (r_i in r_vec){
+    print(r_i)
     r_close_list[[option]][[paste("radius", r_i)]] = list()
     statistic_close_list[[option]][[paste("radius", r_i)]] = list()
-    
+    print(length(all_types$fns))
     for (range in (1:length(all_types$fns))){
+      
       r_close_list[[option]][[paste("radius", r_i)]][[paste(option, "fns which",range)]] = r_i
       
-      
-      if ((option == "K")|| (option == "L") || (option == "Kdot")|| (option == "Ldot")){
-        
-        stat1 = all_types[["fns"]][[range]][["border"]][left]
-        stat2 = all_types[["fns"]][[range]][["border"]][right]
-        print(c(stat1,stat2))
-        
-        a = (stat2-stat1)/(r2-r1) # y=ax+b
-        b = stat2 - a*r2
-        stat = a*(r_i-r2)+stat2
-        statistic_close_list[[option]][[paste("radius", r_i)]][[option]][[paste(option, "fns which",range)]] = stat
-      } else if (option == "pcf"){
-        stat1 = all_types[["fns"]][[range]][["trans"]][left]
-        stat2 = all_types[["fns"]][[range]][["trans"]][right]
-        print(c(stat1,stat2))
-        
-        a = (stat2-stat1)/(r2-r1)
-        b = stat2 - a*r2
-        stat = a*(r_i-r2)+stat2
-        
-        statistic_close_list[[option]][[paste("radius", r_i)]][[option]][[paste(option, "fns which",range)]] = stat
-      } else{
-        
-        view(all_types[["fns"]][[range]])
-        r_emperic = all_types[["fns"]][[range]][["r"]]
+      r_emperic = all_types[["fns"]][[range]][["r"]]
+      dif = r_emperic - r_i
+      dif_abs = abs(dif)
+      condition = match(min(dif_abs),dif_abs)
+  
+      if (dif[condition]>0){
+        left = condition - 1
+        right = condition
+      }else{
+        left = condition
+        right = condition + 1
+      }
 
-        stat_emperic = all_types[["fns"]][[range]][["theohaz"]] #ASSUMPTION temporarily
-
+      r1 = r_emperic[left]
+      r2 = r_emperic[right]
+      if(range == length(all_types$fns)){
+        browser()
+      }
+      if ((option == "K")|| (option == "L") || (option == "Kdot")|| (option == "Ldot") || (option == "pcf")){
         
         if (max(r_emperic)< r_i){
           warning("default r interval is to small")
           next(paste("skip", range, option))
         }
         
-        dif = r_emperic - r_i
-        dif_abs = abs(dif)
-        condition = match(min(dif_abs),dif_abs)
+        stat1 = all_types[["fns"]][[range]][["isotropic"]][left]
+        stat2 = all_types[["fns"]][[range]][["isotropic"]][right]
         
-        if (dif[condition]>0){
-          left = condition - 1
-          right = condition
-        }else{
-          left = condition
-          right = condition + 1
-        }
-        
-        r1 = r_emperic[left]
-        r2 = r_emperic[right]
-        
-        print(c(r1,r2))
-        stat1 = all_types[["fns"]][[range]][["theohaz"]][left]
-        stat2 = all_types[["fns"]][[range]][["theohaz"]][right]
-        
-        # stat1 = all_types[["fns"]][[range]][["rs"]][left]
-        # stat2 = all_types[["fns"]][[range]][["rs"]][right]
-        print(c(stat1,stat2))
-        
-        a = (stat2-stat1)/(r2-r1)
+        a = (stat2-stat1)/(r2-r1) # y=ax+b
         b = stat2 - a*r2
         stat = a*(r_i-r2)+stat2
+        
         statistic_close_list[[option]][[paste("radius", r_i)]][[option]][[paste(option, "fns which",range)]] = stat
         
         print(paste("r left of r_i is", r1, ",r_i is", r_i, ",right of r_i is", r2))
         print(paste("stat left of r_i is", stat1, ",stat is", stat, ",right of stat is", stat2))
-        stop("end")
+        
+        if(range == length(all_types$fns)){
+          browser()
+        }
+        # browser(text = "KdotLdotpcf")
+        
+      } else{
+        
+        if (max(r_emperic)< r_i){
+          warning("default r interval is to small")
+          next(paste("skip", range, option))
+        }
+        
+        stat_emperic = all_types[["fns"]][[range]][["km"]] #ASSUMPTION temporarily
+        # view(all_types[["fns"]][[range]])
+        stat1 = all_types[["fns"]][[range]][["km"]][left]
+        stat2 = all_types[["fns"]][[range]][["km"]][right]
+        
+        a = (stat2-stat1)/(r2-r1)
+        b = stat2 - a*r2
+        stat = a*(r_i-r2)+stat2
+        
+        statistic_close_list[[option]][[paste("radius", r_i)]][[option]][[paste(option, "fns which",range)]] = stat
+
+        print(paste("r left of r_i is", r1, ",r_i is", r_i, ",right of r_i is", r2))
+        print(paste("stat left of r_i is", stat1, ",stat is", stat, ",right of stat is", stat2))
+        
+        if(range == length(all_types$fns)){
+          browser()
+        }
       }
     }
   }
